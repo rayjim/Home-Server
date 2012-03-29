8a6d3c7c08d07ed14ded5aec323e21537e7f1a1c
Rayjim <baoruihan@gmail.com>
2012-03-29 11:46:25 +0900

    from air

---
+++ scripts/my_scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$(".guess_box").click(checkForCode);
+	function getRandom(num){
+		var my_num = Math.floor(Math.random()*num);
+		return my_num;
+	
+	}
+	function hideCode(){
+		var numRand = getRandom(4);
+		$(".guess_box").each(function(index, value){
+			if(numRand == index){
+				$(this).append("<span id ='has_discount'></span>");
+		return false;
+			
+			}
+		});
+	}
+	hideCode();
+	function checkForCode(){
+		var discount;
+		if($.contains(this,document.getElementById("has_discount")))
+		{
+			var my_num = getRandom(5);
+			discount ="<p>Your Discount is "+my_num+"%</p>";
+		}
+		else
+		{
+			discount = "<p>Sorry, no discount this time!</p>";
+		}
+		$(this).append(discount);
+		$(".guess_box").each(function(){
+			$(this).unbind('click');
+		});
+	
+	}
+
+
+});
